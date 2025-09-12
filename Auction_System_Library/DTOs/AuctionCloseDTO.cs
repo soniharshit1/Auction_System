0206079b55a78e80629c467f73a3ec6621e7230b
KshitijA1003 <kshitij.sharma6@cognizant.com>
2025-09-12 17:38:58 +0530

    Created DTOs for Auction Controller.

---
+++ Auction_System_Library/DTOs/AuctionCloseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction_System_Library.DTOs
+{
+    public class AuctionCloseDTO
+    {
+        public decimal FinalBid { get; set; }
+    }
+}
---
+++ Auction_System_Library/DTOs/AuctionCreateDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction_System_Library.DTOs
+{
+    public class AuctionCreateDTO
+    {
+        public int ProductId { get; set; }
+        public int SellerId { get; set; }
+        public decimal StartPrice { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
