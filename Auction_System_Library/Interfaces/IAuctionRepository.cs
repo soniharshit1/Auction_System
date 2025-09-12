671a8f93d2391ec8bfa1977e6bc6ef58a6eb0705
KshitijA1003 <kshitij.sharma6@cognizant.com>
2025-09-12 16:07:25 +0530

    Added interface for Auction model in the Auction System Library.

---
+++ Auction_System_Library/Interfaces/IAuctionRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Auction_System_Library.Models;
+
+namespace Auction_System_Library.Interfaces
+{
+    public interface IAuctionRepository
+    {
+        Task<IEnumerable<Auction>> GetAllAuctionsAsync();
+        Task<IEnumerable<Auction>> GetActiveAuctionsAsync();
+        Task<Auction> GetAuctionByIdAsync(int id);
+        Task<string> CreateAuctionsAsync(Auction auction);
+        Task<string> UpdateAuctionAsync(int id, Auction updatedAuction);
+        Task<string> DeleteAuctionAsync(int id);
+        Task<IEnumerable<Auction>> GetAuctionsBySellerAsync(int sellerId);
+        Task<IEnumerable<Auction>> GetAuctionsByProductAsync(int productId);
+        Task<string> CloseAuctionAsync(int id, decimal finalBid);
+
+    }
+}
---
+++ Auction_System_Library/Repository/AuctionRepository.cs
@@ -0,0 +1,40 @@
+﻿using Auction_System_Library.Data;
+using Auction_System_Library.Interfaces;
+using Auction_System_Library.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction_System_Library.Repository
+{
+    public class AuctionRepository : IAuctionRepository
+
+    {
+        private readonly AuctionDbContext _context;
+
+        public AuctionRepository(AuctionDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Auction>> GetAllAuctionsAsync()
+        {
+            return await _context.Auctions.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Auction>> GetActiveAuctionsAsync()
+        {
+            return await _context.Auctions
+                .Where(a => a.Status == true && a.EndDate > DateTime.Now)
+                .ToListAsync();
+        }
+
+        public async Task<Auction> GetAuctionByIdAsync(int id)
+        {
+            return await _context.Auctions
+        }
+    }
+}
