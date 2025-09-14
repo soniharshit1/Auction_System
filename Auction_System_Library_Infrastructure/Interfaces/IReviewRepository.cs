a00dcc1fa225e8147698f0c50322505c13de5d7e
jyothika-i <Jyothika.Inupakuthika@cognizant.com>
2025-09-14 20:24:40 +0530

    Added IReviewRepository, and DTOs required for Reviews Table

---
+++ Auction_System_Library_Infrastructure/Interfaces/IReviewRepository.cs
@@ -0,0 +1,19 @@
+﻿using Auction_System_Library_Database.Models;
+using Auction_System_Library_Infrastructure.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction_System_Library_Infrastructure.Interfaces
+{
+    public interface IReviewRepository
+    {
+        Task<IEnumerable<Review>> GetAllReviewsAsync();
+        Task<Review> GetReviewByIdAsync(int id);
+        Task AddReviewAsync(CreateReviewDTO dto);
+        Task UpdateReviewAsync(int id, UpdateReviewDTO dto);
+        Task DeleteReviewAsync(int id);
+    }
+}
