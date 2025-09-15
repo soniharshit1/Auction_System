ec2895531b56882632a866d8f345b448f0d4f071
KshitijA1003 <kshitij.sharma6@cognizant.com>
2025-09-15 12:33:09 +0530

    Added BidCreateDTO in the DTOs for Bidcontroller

---
+++ Auction_System_WebApi/Controllers/BidsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Auction_System_Library_Database.Data;
+using Auction_System_Library_Database.Models;
+
+namespace Auction_System_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BidsController : ControllerBase
+    {
+        private readonly AuctionDbContext _context;
+
+        public BidsController(AuctionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Bids
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Bid>>> GetBids()
+        {
+            return await _context.Bids.ToListAsync();
+        }
+
+        // GET: api/Bids/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Bid>> GetBid(int id)
+        {
+            var bid = await _context.Bids.FindAsync(id);
+
+            if (bid == null)
+            {
+                return NotFound();
+            }
+
+            return bid;
+        }
+
+        // PUT: api/Bids/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBid(int id, Bid bid)
+        {
+            if (id != bid.BidId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bid).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BidExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Bids
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Bid>> PostBid(Bid bid)
+        {
+            _context.Bids.Add(bid);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBid", new { id = bid.BidId }, bid);
+        }
+
+        // DELETE: api/Bids/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBid(int id)
+        {
+            var bid = await _context.Bids.FindAsync(id);
+            if (bid == null)
+            {
+                return NotFound();
+            }
+
+            _context.Bids.Remove(bid);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BidExists(int id)
+        {
+            return _context.Bids.Any(e => e.BidId == id);
+        }
+    }
+}
